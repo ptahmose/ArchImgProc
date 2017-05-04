58f84a38eb7b4d0414f04d969df1db2ee1a95165
ptahmose <jbohl@h-quer.de>
2017-05-05 00:22:02 +0200

    test-script

---
+++ TestArrowDetection.ps1
@@ -0,0 +1,15 @@
+﻿$OutputDirBase = "W:\Data\ArrowDetect\"
+$Exename = "D:\DEV\GitHub\ArchImgProc\x64\Debug\LineSegmenterTest.exe"
+$files = Get-ChildItem "W:\Data\Archery-Targets" -Filter *.jpg 
+for ($i=0;$i -lt $files.Count;$i++){
+    $OutDir = $OutputDirBase + [io.path]::GetFileNameWithoutExtension($files[$i]) +"\"
+    Write-Host $OutDir
+    If(!(test-path $OutDir))
+    {
+        New-Item -ItemType Directory -Force -path $OutDir
+    }
+
+    Write-Host $files[$i].FullName
+    &$Exename -s $files[$i].FullName -o $OutDir -p output
+    
+}
